6d43c1d7ee2fa63f8adf9e4faaf89f576dd5a924
t_coding <trapperwetzel@gmail.com>
2025-02-21 20:45:35 -0600

    binarysearch update

--- binarysearch.py
+++ binarysearch.py
@@ -7,29 +7,30 @@ for i in range(0,129):
 
 
 
-def binarysearch(anarray):
 
+def binarysearchfornumber(anarray, target):
     low = 0 
-    high = len(arr1) - 1
+    high = len(anarray) - 1
     count = 0 
-    # how many times does it take for you to search through the entire array? 
+    
+    while (low <= high):
+        count += 1 
+        mid = (high + low) // 2
+        guess = anarray[mid]
+        if (guess == target):
+            return mid, count 
+        if (guess > target):
+            high = mid - 1 
+        elif (guess < target):
+            low = mid + 1
+    return None 
 
-
-    while(low <= high):
-        count += 1
-
-        mid = (low + high) // 2
-        low = mid + 1
-
-    print(count)
-
-
-
-
-
-
-binarysearch(arr1) 
-print()
-print() 
+userinput = ""
+while(userinput != "quit"):
+    print()
+    userinput = input("Press enter to start or type quit to exit\n") 
+    usernum = int(input("Enter number to binary search for!\n"))
+    result,count = binarysearchfornumber(arr1,usernum)
+    print(f"Is your number {result}? If so, then the number was found properly due to binary search! It took {count} tries to get there!") 
 
  
